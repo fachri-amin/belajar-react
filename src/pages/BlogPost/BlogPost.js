b4472cb58ee87d38a10dcefd1d1d5c0b91e7beb8
fachri-amin <fachri.amin@gmail.com>
2020-11-25 14:01:37 +0700

    create post to API method

--- src/pages/BlogPost/BlogPost.js
+++ src/pages/BlogPost/BlogPost.js
@@ -6,16 +6,64 @@ import axios from 'axios';
 class BlogPost extends Component{
 
     state = {
-        post : [],
+        post: [],
+        formBlogPost: {
+            id: 1,
+            title: "",
+            body: "",
+            userId: 1
+        }
     }
 
-    componentDidMount(){
-        axios.get('https://jsonplaceholder.typicode.com/posts')
+    deleteHandler = (id)=> {
+        console.log(id);
+        axios.delete(`https://my-json-server.typicode.com/fachri-amin/fake-api/posts/${id}`)
+        .then((res)=>{
+            if(res.status == 200){
+                alert('Delete Success');
+            }
+            else{
+                alert('Delete Failed');
+            }
+        });
+    }
+
+    getPostAPI = ()=>{
+        axios.get('https://my-json-server.typicode.com/fachri-amin/fake-api/posts')
         .then((result)=>{
             this.setState({
                 post:result.data
             })
-        })            
+        });    
+    }
+
+    componentDidMount(){
+        axios.get('https://my-json-server.typicode.com/fachri-amin/fake-api/posts')
+        .then((result)=>{
+            this.setState({
+                post:result.data
+            })
+        });            
+    }
+
+    formChange = (event)=>{
+        let formNewValue = {...this.state.formBlogPost};
+        let title = event.target.value;
+        let timestamp = new Date().getTime();
+        formNewValue[event.target.name] = title;
+        formNewValue["id"] = timestamp;
+        this.setState({
+            formBlogPost: formNewValue
+        });
+    }
+
+    postAPI = ()=>{
+        axios.post('https://my-json-server.typicode.com/fachri-amin/fake-api/posts', this.state.formBlogPost)
+        .then((res)=>{
+            alert('New post added');
+        }, (err)=>{
+            alert(err);
+        });
     }
 
     render(){
@@ -23,9 +71,22 @@ class BlogPost extends Component{
             <Fragment>
                 <p>Blog Post</p>
                 <hr />
+
+                <form className="col-lg-6 rounded shadow-lg pb-3 mb-5 ml-4">
+                    <div className="form-group">
+                        <label htmlFor="title">Title</label>
+                        <input name="title" type="text" className="form-control" id="title" onChange={this.formChange} />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="body">Body</label>
+                        <textarea name="body" id="body" className="form-control" cols="30" rows="10" onChange={this.formChange}></textarea>
+                    </div>
+                    <button type="button" className="btn btn-primary" onClick={this.postAPI}>Submit</button>
+                </form>
+
                 {
                     this.state.post.map(post => {
-                        return <Post key={post.id} title={post.title} desc={post.body} />
+                        return <Post key={post.id} data={post} remove={(id)=>this.deleteHandler(id)} />
                         {/* props key harus ada agar setiap component punya keunikan, jika tidak maka akan ada warning */}
                     })
                 }
